f4c01184b230a3fecfb63493394ba15907aab28d
Bruna Motta <brunamottap@gmail.com>
2024-12-07 23:05:48 -0300

    usando rotas aninhadas

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Inicio from "./paginas/inicio";
-import SobreMim from "./paginas/sobre mim";
+import Inicio from "./paginas/Inicio";
+import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodapé";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 function AppRoutes() {
   return (
       <BrowserRouter>
-      
+
         <Menu />
         
         <Routes>
-          <Route path="/" element={<Inicio />}/>
-          <Route path="/sobremim" element={<SobreMim />}/>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />}/>
+            <Route path="sobremim" element={<SobreMim />}/>
+          </Route>
+
           <Route path="*" element={<h1>Página não encontrada</h1>}/>
         </Routes>
 
